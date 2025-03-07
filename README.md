e28ee7666a3ea1abe12e0051113d666e724274fc
adamleif <adamleif@gmail.com>
2025-03-07 01:30:12 -0800

    overhauled original upgradeQ code; modified README.md

--- README.md
+++ README.md
@@ -1,37 +1,38 @@
 # Obscounter
 
-Python implementation of hotkey counter in OBS Studio.
+A flexible, hotkey based counter for OBS Studio created in Python.
+
 
 ![prevew](counter.gif)
 
 # Requirements
 
-For Windows install [python3.6](https://www.python.org/downloads/release/python-368/) 64 or 32 bit depending on your OBS. Since 28 version OBS Studio supports most 3.x Python versions.
+For Windows install [python3.6](https://www.python.org/downloads/release/python-368/) 64 or 32 bit depending on your OBS. Since 28 version OBS Studio supports most 3.x Python versions. The most recent version of this code was written using python3.12.
 
 # Installation 
 
-1. Download [source code](https://github.com/upgradeQ/Obscounter/archive/master.zip).
-2. Unzip the file to be able to access `hotkey_counter.py`. Remember the file location because you will need this later.
+1. Download [source code](https://github.com/adamleif/Counter/archive/main.zip).
+2. Unzip the file and place `hotkey_counter.py` in target directory. 
 
 # Usage
 
-1. On OBS, under Sources, click + to create a new text source (below, you will see Text GDI+). You may leave it blank.
+1. In OBS, under Sources, click + to create a new Text GDI+ source. Format as desired. You may leave the text field blank.
 
 ![image](https://user-images.githubusercontent.com/2420577/214267000-44e091a0-eadb-43a2-ac68-d8763b172320.png)
 
-2. On the OBS menu, click `Tools > Scripts`
+2. In the OBS menu, click `Tools > Scripts`
 
 ![image](https://user-images.githubusercontent.com/2420577/214267186-562deac4-ee82-46df-8ebc-5278f9429f64.png)
 
-3. Under the `Python Settings` tab, make sure the Python path is configured.
+3. Under the `Python Settings` tab, make sure your Python path is configured.
 
 ![image](https://user-images.githubusercontent.com/2420577/214267353-7155c08d-f9eb-4053-a17f-34ada6af86f5.png)
 
-4. Under the `Scripts` tab, click the + sign to add this script.
+4. Under the `Scripts` tab, click the + sign to add `hotkey_counter.py` from your previously specified target directory.
 
 ![image](https://user-images.githubusercontent.com/2420577/214267447-cb5de6cc-5b98-44d6-bb5f-cccff76be836.png)
 
-5. Close the scripts window, and return to your OBS window and configure OBS settings by clicking `File > Settings`.
+5. Close the scripts window, return to your OBS window, and configure OBS hotkey settings by clicking `File > Settings > Hotkeys`.
 
 6. Configure the hotkeys.
 
@@ -41,13 +42,7 @@ For Windows install [python3.6](https://www.python.org/downloads/release/python-
 
 ## How do I use more counters?
 
-If you need additional counters, duplicate the `hotkey_counter.py` file and save it with a new filename (e.g. `hotkey_counter2.py`), and repeat the process.
-
-
-# Alternatives
-
-There is a [`Lua alternative`](https://gist.github.com/tid-kijyun/477c723ea42d22903ebe6b6cee3f77a1) with helpful blogpost (JP)
-on how to install and setup those kinds of scripts in OBS. Lua support in OBS Studio is built-in , so you don't need install anything extra.
+If you need additional counters, modify the `Number of Counters` field of the script in the OBS UI; then, refresh the script. Additional counters should appear in the sidebar, and additional hotkeys will be created for those counters.
 
 # Contribute 
 
--- hotkey_counter.py
+++ hotkey_counter.py
@@ -1,17 +1,35 @@
 import obspython as S
 
-__version__ = "1.1.0"
+__version__ = "2.0.0"  # because nearly everything was rewritten, I suppose
 
+N = 2  # default number of counters; can be updated by OBS input
 
 class TextContent:
     def __init__(self, source_name=None, text_string="This is default text"):
+        """
+        Initializes the TextContent object.
+
+        :param source_name: the name of the OBS source associated with the 
+                            counter.
+        :param text_string: the default text to be displayed.
+        """
         self.source_name = source_name
         self.text_string = text_string
         self.counter = 0
 
     def update_text(self, counter_text, counter_value=0):
+        """
+        Updates the text of the associated OBS source.
+
+        :param counter_text: the prefix text for the counter 
+                             (e.g., "Counter: ").
+        :param counter_value: the value to update the counter by (increment, 
+                              decrement, or reset).
+        """
         source = S.obs_get_source_by_name(self.source_name)
         settings = S.obs_data_create()
+
+        # update the counter based on the counter_value
         if counter_value == 1:
             self.counter += 1
         if counter_value == -1:
@@ -23,6 +41,7 @@ class TextContent:
 
         self.text_string = f"{counter_text}{self.counter}"
 
+        # apply the updated text to the source
         S.obs_data_set_string(settings, "text", self.text_string)
         S.obs_source_update(source, settings)
         S.obs_data_release(settings)
@@ -40,11 +59,23 @@ class Driver(TextContent):
         self.update_text(self.counter_text, 0)
 
     def do_custom(self, val):
+        """
+        Sets a custom value for the counter.
+
+        :param val: the custom value to set for the counter.
+        """
         self.update_text(self.counter_text, str(val))
 
 
 class Hotkey:
     def __init__(self, callback, obs_settings, _id):
+        """
+        Initializes the Hotkey object.
+
+        :param callback: the function to call when the hotkey is pressed.
+        :param obs_settings: the OBS settings data to save/load hotkey configs.
+        :param _id: the unique identifier for this hotkey.
+        """
         self.obs_data = obs_settings
         self.hotkey_id = S.OBS_INVALID_HOTKEY_ID
         self.hotkey_saved_key = None
@@ -56,6 +87,7 @@ class Hotkey:
         self.save_hotkey()
 
     def register_hotkey(self):
+        """Registers the hotkey with OBS."""
         description = "Htk " + str(self._id)
         self.hotkey_id = S.obs_hotkey_register_frontend(
             "htk_id" + str(self._id), description, self.callback
@@ -63,12 +95,14 @@ class Hotkey:
         S.obs_hotkey_load(self.hotkey_id, self.hotkey_saved_key)
 
     def load_hotkey(self):
+        """Loads the saved hotkey configuration from OBS settings."""
         self.hotkey_saved_key = S.obs_data_get_array(
             self.obs_data, "htk_id" + str(self._id)
         )
         S.obs_data_array_release(self.hotkey_saved_key)
 
     def save_hotkey(self):
+        """Saves the current hotkey configuration to OBS settings."""
         self.hotkey_saved_key = S.obs_hotkey_save(self.hotkey_id)
         S.obs_data_set_array(
             self.obs_data, "htk_id" + str(self._id), self.hotkey_saved_key
@@ -77,147 +111,213 @@ class Hotkey:
 
 
 class HotkeyDataHolder:
-    htk_copy = None  # this attribute will hold instance of Hotkey
-
-
-hotkeys_counter_1 = Driver()
-hotkeys_counter_2 = Driver()
-
-h01 = HotkeyDataHolder()
-h02 = HotkeyDataHolder()
-h03 = HotkeyDataHolder()
-h11 = HotkeyDataHolder()
-h12 = HotkeyDataHolder()
-h13 = HotkeyDataHolder()
-# ------------------------------------------------------------------------------
-
-
-def callback_up1(pressed):
-    if pressed:
-        return hotkeys_counter_1.increment()
-
-
-def callback_down1(pressed):
-    if pressed:
-        return hotkeys_counter_1.decrement()
-
-
-def callback_custom1(*args):
-    hotkeys_counter_1.do_custom(S.obs_data_get_int(args[2], "counter_1"))
-    return True
-
-
-def callback_reset1(pressed):
-    if pressed:
-        return hotkeys_counter_1.reset()
-
-
-def callback_up2(pressed):
-    if pressed:
-        return hotkeys_counter_2.increment()
-
-
-def callback_down2(pressed):
-    if pressed:
-        return hotkeys_counter_2.decrement()
-
-
-def callback_reset2(pressed):
-    if pressed:
-        return hotkeys_counter_2.reset()
-
-
-def callback_custom2(*args):
-    hotkeys_counter_2.do_custom(S.obs_data_get_int(args[2], "counter_2"))
-    return True
+    """
+    Holder for the Hotkey instances for each counter.
+    """
+    htk_copy = None
 
 
 def script_description():
-    return "COUNTER 2"
+    return "A flexible, hotkey based counter for OBS Studio."
+
+
+# define counters dynamically based on N
+counters = {i: Driver() for i in range(N)}  # Create N counters
+
+# create HotkeyDataHolder instances (increment, decrement, reset), 3/counter
+data_holders = {i:{j:HotkeyDataHolder() for j in range(3)} for i in 
+                counters.keys()}
+
+# -----------------------------------------------------------------------------
+
+def create_callback_up(c):
+    """
+    Creates a callback for incrementing a specific counter.
+
+    :param c: the index of the counter to increment.
+    :return: a callback function for the increment hotkey.
+    """
+    def callback(pressed):
+        if pressed: counters[c].increment()
+        
+    return callback
+
+
+def create_callback_down(c):
+    """
+    Creates a callback for decrementing a specific counter.
+
+    :param c: the index of the counter to decrement.
+    :return: a callback function for the decrement hotkey.
+    """
+    def callback(pressed):
+        if pressed: counters[c].decrement()
+
+    return callback
+
+
+def create_callback_reset(c):
+    """
+    Creates a callback for resetting a specific counter.
+
+    :param c: the index of the counter to reset.
+    :return: a callback function for the reset hotkey.
+    """
+    def callback(pressed):
+        if pressed: counters[c].reset()
+
+    return callback
+
+
+def callback_custom(props, prop, settings, c):
+    """
+    Callback for setting a custom value for the counter when the custom input 
+    field is modified.
+
+    :param props: the properties for the script. Not utilized.
+    :param prop: the specific property that was modified. Not utilized.
+    :param settings: the settings for the script.
+    :param c: the index of the counter being modified.
+    """
+    value = S.obs_data_get_int(settings, f"counter_{c}")
+    counters[c].do_custom(value)
+    
+    return True
 
 
 def script_update(settings):
-    hotkeys_counter_1.source_name = S.obs_data_get_string(settings, "source1")
-    hotkeys_counter_1.counter_text = S.obs_data_get_string(settings, "counter_text1")
+    """
+    Updates the script settings with the values from the OBS settings.
 
-    hotkeys_counter_2.source_name = S.obs_data_get_string(settings, "source2")
-    hotkeys_counter_2.counter_text = S.obs_data_get_string(settings, "counter_text2")
+    :param settings: the updated settings for the script.
+    """
+    global N
+    
+    # update N based on user input
+    N = S.obs_data_get_int(settings, "num_counters")
+
+    # re-create counters dynamically based on the new value of N
+    global counters
+    counters = {i: Driver() for i in range(N)}
+
+    for c in counters:
+        counters[c].source_name = S.obs_data_get_string(
+            settings, f"source_{c}")
+        counters[c].counter_text = S.obs_data_get_string(
+            settings, f"counter_text{c}")
 
 
 def script_properties():
+    """
+    Defines the properties (inputs) for the script configuration in the OBS UI.
+
+    :return: the properties for the script.
+    """
     props = S.obs_properties_create()
+    ps = []
 
-    S.obs_properties_add_text(
-        props, "counter_text1", "[1]Set counter text", S.OBS_TEXT_DEFAULT
-    )
-    p = S.obs_properties_add_int(
-        props, "counter_1", "Set custom value", -999999, 999999, 1
-    )
-    S.obs_property_set_modified_callback(p, callback_custom1)
-    p1 = S.obs_properties_add_list(
-        props,
-        "source1",
-        "[1]Text Source",
-        S.OBS_COMBO_TYPE_EDITABLE,
-        S.OBS_COMBO_FORMAT_STRING,
+    # add a property to set the number of counters dynamically
+    S.obs_properties_add_int(
+        props, "num_counters", "Number of Counters", 1, 100, 1
     )
 
-    S.obs_properties_add_text(
-        props, "counter_text2", "[2]Set counter text", S.OBS_TEXT_DEFAULT
-    )
-    p = S.obs_properties_add_int(
-        props, "counter_2", "Set custom value", -999999, 999999, 1
-    )
-    S.obs_property_set_modified_callback(p, callback_custom2)
-    p2 = S.obs_properties_add_list(
-        props,
-        "source2",
-        "[2]Text Source",
-        S.OBS_COMBO_TYPE_EDITABLE,
-        S.OBS_COMBO_FORMAT_STRING,
-    )
+    for c in counters:
+        # add the counter text field for each counter
+        S.obs_properties_add_text(
+            props, f"counter_text{c}", f"[{c}] Counter Frontmatter",
+            S.OBS_TEXT_DEFAULT
+        )
+
+        # add the custom value input field for each counter
+        p = S.obs_properties_add_int(
+            props, f"counter_{c}", f"[{c}] Set Custom Value", -999999, 999999, 
+            1
+        )
+
+        # set the callback for the custom value input field
+        S.obs_property_set_modified_callback(
+            p, lambda props, prop, settings, c=c: 
+                callback_custom(props, prop, settings, c)
+        )
+
+        # add the text source dropdown for each counter
+        ps.append(
+            S.obs_properties_add_list(
+                props,
+                f"source_{c}",
+                f"[{c}] Text Source",
+                S.OBS_COMBO_TYPE_EDITABLE,
+                S.OBS_COMBO_FORMAT_STRING,
+            )
+        )
+
+    # populate the source dropdown with available text sources in OBS
     sources = S.obs_enum_sources()
     if sources is not None:
         for source in sources:
             source_id = S.obs_source_get_unversioned_id(source)
             if source_id == "text_gdiplus" or source_id == "text_ft2_source":
                 name = S.obs_source_get_name(source)
-                S.obs_property_list_add_string(p1, name, name)
-                S.obs_property_list_add_string(p2, name, name)
+                for p in ps:
+                    S.obs_property_list_add_string(p, name, name)
 
         S.source_list_release(sources)
+
     return props
 
 
 def script_load(settings):
-    hotkeys_counter_1.counter = S.obs_data_get_int(settings, "counter1")
-    hotkeys_counter_2.counter = S.obs_data_get_int(settings, "counter2")
-    h01.htk_copy = Hotkey(callback_up1, settings, "count_up1")
-    h02.htk_copy = Hotkey(callback_down1, settings, "count_down1")
-    h03.htk_copy = Hotkey(callback_reset1, settings, "reset1")
+    """
+    Loads the settings for the script, including counter values and hotkey 
+    configurations.
 
-    h11.htk_copy = Hotkey(callback_up2, settings, "count_up2")
-    h12.htk_copy = Hotkey(callback_down2, settings, "count_down2")
-    h13.htk_copy = Hotkey(callback_reset2, settings, "reset2")
+    :param settings: The settings to load.
+    """
+    global N
+    N = S.obs_data_get_int(settings, "num_counters")  # load N from settings
+
+    # re-create counters based on the new value of N
+    global counters
+    counters = {i: Driver() for i in range(N)}
+
+    for c in counters:
+        counters[c].counter = S.obs_data_get_int(settings, f"counter_{c}")
+        data_holders[c][0].htk_copy = Hotkey(
+            create_callback_up(c), settings, f"count_up{c}")
+        data_holders[c][1].htk_copy = Hotkey(
+            create_callback_down(c), settings, f"count_down{c}")
+        data_holders[c][2].htk_copy = Hotkey(
+            create_callback_reset(c), settings, f"count_reset{c}")
 
 
 def script_save(settings):
-    S.obs_data_set_int(settings, "counter1", hotkeys_counter_1.counter)
-    S.obs_data_set_int(settings, "counter2", hotkeys_counter_2.counter)
-    for h in [h01, h02, h03, h11, h12, h13]:
-        h.htk_copy.save_hotkey()
+    """
+    Saves the settings for the script, including counter values and hotkey 
+    configurations.
+
+    :param settings: The settings to save.
+    """
+    for c in counters:
+        S.obs_data_set_int(settings, f"counter_{c}", counters[c].counter)
+        for h in data_holders[c]:
+            data_holders[c][h].htk_copy.save_hotkey()
 
 
 description = """
 <h2>Version : {__version__}</h2>
-<a href="https://github.com/upgradeQ/Obscounter"> Webpage </a>
+<a href="https://github.com/adamleif/Counter"> Webpage </a>
 <h3 style="color:orange">Authors</h3>
 <a href="https://github.com/upgradeQ"> upgradeQ </a> <br>
-""".format(
-    **locals()
-)
+<a href="https://github.com/adamleif"> adamleif </a> <br>
+""".format(**locals())
 
 
 def script_description():
+    """
+    Returns the description of the script, including version and author 
+    information.
+
+    :return: The description of the script.
+    """
     print(description, "Released under MIT license")
     return description
